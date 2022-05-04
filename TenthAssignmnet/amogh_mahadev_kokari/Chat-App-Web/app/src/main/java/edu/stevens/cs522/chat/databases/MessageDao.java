497a69ae5dad7245e428ee6e042ef289b1225b27
amoghkokari <amoghsrcm@gmail.com>
2022-05-04 18:03:58 -0400

    10th assignment

---
+++ TenthAssignmnet/amogh_mahadev_kokari/Chat-App-Web/app/src/main/java/edu/stevens/cs522/chat/databases/MessageDao.java
@@ -0,0 +1,30 @@
+package edu.stevens.cs522.chat.databases;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Transaction;
+import androidx.room.Update;
+
+import java.util.List;
+
+import edu.stevens.cs522.chat.entities.Chatroom;
+import edu.stevens.cs522.chat.entities.Message;
+import edu.stevens.cs522.chat.entities.Peer;
+
+// TODO add annotations for Repository pattern
+@Dao
+public interface MessageDao {
+
+    @Query("select * from message WHERE message.chatroom = :chatroom")
+    public abstract LiveData<List<Message>> fetchAllMessages(String chatroom);
+
+    @Query("SELECT * FROM message  WHERE message.sender = :peerName")
+    public LiveData<List<Message>> fetchMessagesFromPeer(String peerName);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    public void persist(Message message);
+
+}
